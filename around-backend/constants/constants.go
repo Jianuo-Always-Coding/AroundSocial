4875a307be4af4691245f4a45520140a02708c17
Jianuo-Always-Coding <jianuo@umich.edu>
2022-10-24 07:26:45 -0400

    A small web demo

---
+++ around-backend/constants/constants.go
@@ -0,0 +1,10 @@
+package constants
+
+const (
+    USER_INDEX = "user"
+    POST_INDEX = "post"
+    ES_URL = "http://10.138.0.2:9200"
+    ES_USERNAME = "jianuo"
+    ES_PASSWORD = "123456"
+    GCS_BUCKET = "jianuo-bucket"
+)
